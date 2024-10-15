7f0a57f51ca5f2f7f0eb57b1756a9d226c85a85e
João Pedro <senac.jotapedro@gmail.com>
2024-10-15 17:22:13 -0300

    update: feito arquivo teste

---
+++ teste.js
@@ -0,0 +1,93 @@
+// for in
+
+// const pessoa = {
+//     nome: 'Joao',
+//     idade: 30,
+//     cidade: 'São Paulo'
+// }
+
+
+// for(let chave in pessoa){
+//     // console.log(`${chave}: ${pessoa[chave]}`);
+//     if (pessoa.nome === 'Joao') {
+//         console.log(`${chave}: ${pessoa[chave]}`);
+//     }else{
+//         console.log('Usuário Não existe')
+//     }
+// }
+
+// for of
+
+// const numeros = [10,20,30];
+
+// percorre o array, mas não itera os objetos
+// const numeros = [
+//     {
+//         numero: 1
+//     }, 
+//     {
+//         numero: 2
+//     }
+// ];
+
+// for(let numero of numeros){
+//     console.log(numero);
+// }
+
+// forEach
+
+// const frutas = ['maca', 'banana', 'laranja'];
+
+// frutas.forEach((fruta) => {
+//     console.log(`${fruta}`);
+// });
+
+// const numeros = [1,2,3,4];
+
+// const dobro = numeros.map(
+//     function dobro(num){
+//         return num * 2;
+//     }
+// );
+
+// console.log(dobro);
+
+// const numeros = [1,2,3,4];
+
+// const pares = numeros.filter(
+
+//     function retornarPares(n){
+//         return n % 2 == 0;
+//     }
+// );
+
+// console.log(pares);
+
+// const numeros = [1,2,3,4];
+
+// const soma = numeros.reduce(
+//     function somar(acumulador, num) {
+//         return acumulador + num;
+//     },0
+// );
+
+// console.log(soma);
+
+// let contador = 0;
+
+// while (contador < 5) {
+//     if (contador % 2 == 0) {
+//         console.log(contador)
+//     }
+
+//     contador++
+// }
+
+const prompt = require('prompt-sync')();
+
+let valor;
+
+
+do {
+    valor = prompt('Digite um valor');
+} while (valor !== 'sair');
